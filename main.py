b0fa81f6587e761476840172c71d3d7201ee5482
Anders bloch lauridsen <93762569+andersbloch09@users.noreply.github.com>
2023-08-13 23:54:08 +0200

    comments made

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ import random
 
 pygame.font.init()
 
+# Width and Height of window 
 WIDTH, HEIGHT = 1800, 800
 
+# Here are all the used global variables 
 FPS = 60
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 PLAYER_VEL = 15
 SCREEN_VEL = 6
-screen_starter = 0
 SPRITE_VEL = SCREEN_VEL
+screen_starter = 0
 hit_occured = False 
 avoid_object_list = []
 distance_between_poles = 0
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #Makes the first display and its size
-pygame.display.set_caption('Player friendly.') # Makes caption 
+# Makes the first display and its size
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) 
+# Makes caption 
+pygame.display.set_caption('Player friendly.')
 
+# Player size of rect
 player_WIDTH = 25
 player_HEIGHT = 25
 
@@ -28,10 +33,11 @@ player = pygame.Rect(0, 0, player_WIDTH, player_HEIGHT)
 #This is the main background 
 GREEN_WORLD = pygame.transform.scale(pygame.image.load('Assets\GreenWorld.PNG'), (WIDTH, HEIGHT)) 
 
+# Events based on the game progress
 PLAYER_HIT = pygame.USEREVENT + 1
 
-
-class avoid_object(pygame.sprite.Sprite): 
+# Class for creation of sprites for poles 
+class avoid_object(pygame.sprite.Sprite):
     def __init__(self, width, height, pos_x, pos_y, color):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
@@ -42,32 +48,29 @@ class avoid_object(pygame.sprite.Sprite):
 
     def update_speed(self):
         global screen_starter
-        if screen_starter >= 1 or player.x > 100: 
+        if screen_starter >= 1 or player.x > 100:
             self.rect.x -= self.speed
             if self.rect.x <= -10:
                 self.kill()
                 avoid_object_list.pop()
 
+# Function which creates the sprites both on bottom and on top
 def sprite_creation_top(first_lvl_group):
     global avoid_object_list
     global distance_between_poles
-    #if len(object_list) < 10:
     # Top Sprite 
     sprite_width = 20
     sprite_pos_y = 0 - 10
     sprite_pos_x = WIDTH - distance_between_poles
-
-    # Starting variables for objects 
-    # Here the objects are written
     sprite_height = random.randint(100, 350)
+    # This variable desides the distance between the poles
     distance_between_poles -= 300
     object_number = avoid_object(sprite_width, sprite_height, sprite_pos_x, sprite_pos_y, (BLACK))
     avoid_object_list.append(object_number)
 
     # Bottom Sprite
     sprite_width = 20
-    # Starting variables for objects 
-    # Here the objects are written
+    # This line is made to get the correct distance between the poles on each side
     sprite_height = 800 - (sprite_height + 75)
     
     sprite_pos_y = HEIGHT - sprite_height + 20
@@ -76,32 +79,33 @@ def sprite_creation_top(first_lvl_group):
     first_lvl_group.add(*avoid_object_list)
     
     return(first_lvl_group)
-        
 
+# This function uses a callback to the class to update the individual sprite location
 def sprite_movement(first_lvl_group):
     for sprite in first_lvl_group:
         sprite.update_speed()
 
+# Draws the content on the window
 def draw_window(player, green_world_move, first_lvl_group, start_line):
     WIN.fill((BLACK))
     WIN.blit(GREEN_WORLD, (green_world_move.x, green_world_move.y))
-   
     # The two if statements resolve in the moving background refresh
     if green_world_move.x < 0: 
         WIN.blit(GREEN_WORLD, (green_world_move.x + WIDTH, green_world_move.y))
     if green_world_move.x == -WIDTH:
         green_world_move.x = 0
+    # This draws the player 
     pygame.draw.rect(WIN, BLACK, player)
-
+    # This draws the start line 
     if start_line.x > -10:
         pygame.draw.rect(WIN, RED, start_line)
-    
+    # This updates the sprites 
     first_lvl_group.update()
     first_lvl_group.draw(WIN)
-    
+    # This updates everything onto the screen
     pygame.display.update()
 
-
+# This function creates the movement for the player
 def player_movement(keys_pressed, player): 
     if keys_pressed[pygame.K_a] and player.x > 0:
         player.x -= PLAYER_VEL
@@ -112,16 +116,16 @@ def player_movement(keys_pressed, player):
     if keys_pressed[pygame.K_s] and player.y < HEIGHT - player_HEIGHT:
         player.y += PLAYER_VEL
 
-
+# This function decides when the screen will move and how fast 
 def screen_movement(player, green_world_move):
     global screen_starter
     if screen_starter >= 1 or player.x > 100: 
        green_world_move.x -= SCREEN_VEL
        screen_starter += 1
 
+# This function check colission of objects and the player 
 def player_hit(first_lvl_group, player):
     global hit_occured
-    #print(hit_occured)
     collided_sprites = [sprite 
                         for sprite in first_lvl_group 
                         if player.colliderect(sprite.rect)]
@@ -130,8 +134,6 @@ def player_hit(first_lvl_group, player):
         pygame.event.post(pygame.event.Event(PLAYER_HIT))
     elif hit_occured and len(collided_sprites) < 1: 
         hit_occured = False
-    #print(len(collided_sprites))
-    #print(hit_occured)
 
 def main():
     global screen_starter
@@ -153,12 +155,12 @@ def main():
                 hit_count += 1
                 print(hit_count)
         
+        # These lines are for the sprites creation and updates 
         if screen_starter >= 1 or player.x > 100: 
             first_lvl_group = sprite_creation_top(first_lvl_group)
             sprite_movement(first_lvl_group)
             start_line.x -= SCREEN_VEL
         
-
         keys_pressed = pygame.key.get_pressed()
         player_hit(first_lvl_group, player)
         player_movement(keys_pressed, player)

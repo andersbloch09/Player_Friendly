f7a760f0a8689ccc316151337a27c263906a7424
Anders bloch lauridsen <93762569+andersbloch09@users.noreply.github.com>
2023-08-14 00:51:53 +0200

    Next work will involve graphics

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ FPS = 60
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-PLAYER_VEL = 15
+PLAYER_VEL = 12
 SCREEN_VEL = 6
 SPRITE_VEL = SCREEN_VEL
 screen_starter = 0
 hit_occured = False 
-avoid_object_list = []
 distance_between_poles = 0
+avoid_object_list = []
 
 # Makes the first display and its size
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) 
@@ -36,6 +36,10 @@ GREEN_WORLD = pygame.transform.scale(pygame.image.load('Assets\GreenWorld.PNG'),
 # Events based on the game progress
 PLAYER_HIT = pygame.USEREVENT + 1
 
+# Creates Text fonts 
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+LOSE_FONT = pygame.font.SysFont('comicsans', 100)
+
 # Class for creation of sprites for poles 
 class avoid_object(pygame.sprite.Sprite):
     def __init__(self, width, height, pos_x, pos_y, color):
@@ -52,16 +56,17 @@ class avoid_object(pygame.sprite.Sprite):
             self.rect.x -= self.speed
             if self.rect.x <= -10:
                 self.kill()
-                avoid_object_list.pop()
+                
 
 # Function which creates the sprites both on bottom and on top
-def sprite_creation_top(first_lvl_group):
-    global avoid_object_list
+def sprite_creation(first_lvl_group):
     global distance_between_poles
+    global avoid_object_list
     # Top Sprite 
     sprite_width = 20
     sprite_pos_y = 0 - 10
     sprite_pos_x = WIDTH - distance_between_poles
+    print(distance_between_poles)
     sprite_height = random.randint(100, 350)
     # This variable desides the distance between the poles
     distance_between_poles -= 300
@@ -76,7 +81,8 @@ def sprite_creation_top(first_lvl_group):
     sprite_pos_y = HEIGHT - sprite_height + 20
     object_number = avoid_object(sprite_width, sprite_height, sprite_pos_x, sprite_pos_y, (BLACK))
     avoid_object_list.append(object_number)
-    first_lvl_group.add(*avoid_object_list)
+    for sprite in avoid_object_list:
+        first_lvl_group.add(sprite)
     
     return(first_lvl_group)
 
@@ -86,7 +92,7 @@ def sprite_movement(first_lvl_group):
         sprite.update_speed()
 
 # Draws the content on the window
-def draw_window(player, green_world_move, first_lvl_group, start_line):
+def draw_window(player, green_world_move, first_lvl_group, start_line, player_health):
     WIN.fill((BLACK))
     WIN.blit(GREEN_WORLD, (green_world_move.x, green_world_move.y))
     # The two if statements resolve in the moving background refresh
@@ -102,6 +108,13 @@ def draw_window(player, green_world_move, first_lvl_group, start_line):
     # This updates the sprites 
     first_lvl_group.update()
     first_lvl_group.draw(WIN)
+    
+    # Displays health 
+    player_health = HEALTH_FONT.render(
+        "Health: " + str(player_health), 1, WHITE)
+
+    WIN.blit(player_health, (10, 10))
+
     # This updates everything onto the screen
     pygame.display.update()
 
@@ -135,12 +148,29 @@ def player_hit(first_lvl_group, player):
     elif hit_occured and len(collided_sprites) < 1: 
         hit_occured = False
 
+def draw_lose(player, green_world_move, first_lvl_group, start_line, player_health):
+    draw_window(player, green_world_move, first_lvl_group, start_line, player_health)
+    
+    draw_text = LOSE_FONT.render("LOSER!", 1, RED)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
+                         2, HEIGHT/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
+
 def main():
     global screen_starter
+    global player
+    global distance_between_poles
+    avoid_object_list.clear()
+    distance_between_poles = 0
+    screen_starter = 0
+    player = pygame.Rect(0, 0, player_WIDTH, player_HEIGHT)
     start_line = pygame.Rect(100, 0, 10, 800)
     first_lvl_group = pygame.sprite.Group()
     green_world_move = pygame.Rect(0, 0, WIDTH, HEIGHT)
     hit_count = 0
+    player_health = 1
+
     clock = pygame.time.Clock()
     run = True
     
@@ -153,11 +183,15 @@ def main():
         
             if event.type == PLAYER_HIT:
                 hit_count += 1
+                player_health -= 1
                 print(hit_count)
-        
+        # Draw if you lose
+        if player_health <= 0: 
+            draw_lose(player, green_world_move, first_lvl_group, start_line, player_health)
+            break
         # These lines are for the sprites creation and updates 
         if screen_starter >= 1 or player.x > 100: 
-            first_lvl_group = sprite_creation_top(first_lvl_group)
+            first_lvl_group = sprite_creation(first_lvl_group)
             sprite_movement(first_lvl_group)
             start_line.x -= SCREEN_VEL
         
@@ -165,7 +199,9 @@ def main():
         player_hit(first_lvl_group, player)
         player_movement(keys_pressed, player)
         screen_movement(player, green_world_move)
-        draw_window(player, green_world_move, first_lvl_group, start_line)
-        
+        draw_window(player, green_world_move, first_lvl_group, start_line, player_health)
+
+    main()
+
 if __name__ == "__main__":
     main()
